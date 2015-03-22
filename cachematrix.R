9099fe61e97fe39eae2de10b59a21b0c4d888336
educal <edumet@hotmail.com>
2015-03-22 11:44:23 +0100

    assignment2
    assignment2 R programing

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+# edu 20150322
 # makeCacheMatrix is a function that returns a list of functions
 # Its puspose is to store a martix and a cached value of the inverse of the 
 # matrix. Contains the following functions:
